e86c5ef586ffbf071c5ee2a06c853e90555a2e62
Midhun K M <midhunkm1294.stark@gmail.com>
2020-05-23 13:15:17 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -49,7 +49,7 @@ for name in files:
 
 import userbot._core
 
-print("Yay! your userbot is officially working. Enjoy! Bot by SnapDragon. Mod by @xditya. Do join  @TeleBotHelp")
+print("Yes! your userbot is officially working. Enjoy! Bot by MarioDevs. @FRIDAYSUPPORTOFFICIAL")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
